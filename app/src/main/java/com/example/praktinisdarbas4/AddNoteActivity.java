feac8922d646803aff032c00fba7242ee9ee7654
lauratmanaviciute <lauraa.atm@gmail.com>
2023-05-07 20:30:53 +0300

    Text Watcher word count when typing note content.

--- app/src/main/java/com/example/praktinisdarbas4/AddNoteActivity.java
+++ app/src/main/java/com/example/praktinisdarbas4/AddNoteActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.HashMap;
@@ -15,6 +18,7 @@ import java.util.Set;
 public class AddNoteActivity extends AppCompatActivity {
     private EditText txtNoteName;
     private EditText txtNoteContent;
+    private TextView tvWordCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,27 @@ public class AddNoteActivity extends AppCompatActivity {
         txtNoteName.setInputType(524288);
         txtNoteContent = findViewById(R.id.txtNoteContent);
         txtNoteContent.setInputType(524288);
+        tvWordCounter = findViewById(R.id.tvWordCounter);
+
+        txtNoteContent.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int count, int after) {
+                String text = txtNoteContent.getText().toString();
+                text = text.replace("\n", " ");
+                String[] textArray = text.split("\\s+");
+                tvWordCounter.setText("Words: " + textArray.length);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     public void onAddNoteClick(View view) {
